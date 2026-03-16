c1d007b2ffda6441e95e03ac77156b79286274ee
GuoYudi <yikezaiting@163.com>
2026-03-16 13:07:05 +0800

    fix(olsrv2): 修复test_runner头文件

---
+++ mysrc/olsrv2/test/test_runner.h
@@ -0,0 +1,10 @@
+#ifndef TEST_RUNNER_H
+#define TEST_RUNNER_H
+
+int run_nhdp_tests();
+int run_olsrv2_tests();
+int run_routing_tests();
+int run_rfc5444_tests();
+int run_tc_tests();
+
+#endif // TEST_RUNNER_H
